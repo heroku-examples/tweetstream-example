9b4a60e4fdbdd0d159a302b8d81b8ce77780ff10
Terence Lee <hone02@gmail.com>
2011-04-26 13:48:01 -0500

    stop creating pidfiles

--- twittah.rb
+++ twittah.rb
@@ -2,6 +2,19 @@ require 'rubygems'
 require 'bundler/setup'
 require 'tweetstream'
 
+# need to stop pidfile creation on heroku (read only file-system)
+class TweetStream::Daemon
+  def start(path, query_parameters = {}, &block) #:nodoc:
+    # Because of a change in Ruvy 1.8.7 patchlevel 249, you cannot call anymore
+    # super inside a block. So I assign to a variable the base class method before
+    # the Daemons block begins.
+    startmethod = super.start
+    Daemons.run_proc(@app_name || 'tweetstream', :multiple => true, :no_pidfiles => true) do
+      startmethod(path, query_parameters, &block)
+    end
+  end
+end
+
 TweetStream::Daemon.new(ENV['TWITTER_USERNAME'], ENV['TWITTER_PASSWORD']).track('OH', 'twitpic', 'http') do |status|
   puts status.text
 end
